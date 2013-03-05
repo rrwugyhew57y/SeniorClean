6ff517460f2ac0644ba7c06178bd6b5986e09946
Kevin Lawrence <kevin.lawrence@brentwood.bc.ca>
2013-03-05 10:37:26 -0800

    Initial Check-in

---
+++ src/seniorclean/Robot.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seniorclean;
+
+/**
+ *
+ * @author kevin.lawrence
+ */
+public class Robot {
+    
+}
---
+++ src/seniorclean/SeniorClean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seniorclean;
+
+/**
+ *
+ * @author kevin.lawrence
+ */
+public class SeniorClean {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
